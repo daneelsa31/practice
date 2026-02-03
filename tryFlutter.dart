40acc0a338f59ff5d639597d3078b2d2dba674f3
daneelsa <daneelsaa@gmail.com>
2026-02-03 21:36:21 +0800

    First Commit

---
+++ tryFlutter.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Sample'),
+        ),
+        body: const Center(
+          child: Text(
+            'Hello Secure World 🔐',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
