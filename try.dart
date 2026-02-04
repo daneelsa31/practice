23de47db419c571b75bad183bc3cbd174da4ff3b
jspe1ba14 <jspe1ba14@scs.local>
2026-02-03 18:53:54 -0800

    First Commit

---
+++ try.dart
@@ -0,0 +1,18 @@
+// main.dart
+void main() {
+  print('Welcome to my Dart application!');
+
+  var user = User(name: 'Alex', age: 25);
+  user.greet();
+}
+
+class User {
+  String name;
+  int age;
+
+  User({required this.name, required this.age});
+
+  void greet() {
+    print('Hi, my name is $name and I am $age years old.');
+  }
+}
